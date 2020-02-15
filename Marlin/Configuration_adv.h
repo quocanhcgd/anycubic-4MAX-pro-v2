156a74ae8a4a3b3c6a9253357ea4f7e242b912ee
Bernhard Berger <bernhard.berger@gmail.com>
2020-02-15 22:02:32 +0100

    Re-Enable LIN_ADVANCE but set the default value to 0.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1501,9 +1501,9 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-//#define LIN_ADVANCE
+#define LIN_ADVANCE           // enable LIN_ADVANCE but set it to 0.
 #if ENABLED(LIN_ADVANCE)
-  #define LIN_ADVANCE_K 0.07	// Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0    	// Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG          // If enabled, this will generate debug information output over USB.
 #endif
 
