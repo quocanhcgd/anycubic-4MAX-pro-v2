0c3fca5217dcd18f99eea5b5b05168655cc5ad0d
Jonas Plamann <jonasp@six.de>
2020-02-17 19:55:50 +0100

    set autotune pid values for the bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -538,10 +538,10 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  //Anycubic i3 Mega Ultrabase (0.9Ω @ 22°C)
-  #define DEFAULT_bedKp 439.45
-  #define DEFAULT_bedKi 73.49
-  #define DEFAULT_bedKd 656.97
+  //Anycubic 4Max Pro Ultrabase (0.9Ω @ 22°C)
+  #define DEFAULT_bedKp 143.88
+  #define DEFAULT_bedKi 34.88
+  #define DEFAULT_bedKd 619.44
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
