93d58ea997d433f5dff3c3e2cf6fcbd307671058
Bernhard Berger <bernhard.berger@gmail.com>
2020-02-20 00:46:21 +0100

    use correct calculated junction_deviation_value based on the stock accel and jerk settings
    (cherry picked from commit b1d383aeb956d8a9aa180cc9191c61962a10fb54)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -813,7 +813,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.038 // (mm) Distance from real junction edge - 0.4 * (jerk^2 / accel_printing)
 #endif
 
 /**
