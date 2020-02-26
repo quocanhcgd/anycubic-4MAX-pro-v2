158709bd6ca4b7ce5be380f422bc54c7b618de1d
Jonas Plamann <jonasp@six.de>
2020-02-26 18:42:13 +0100

    fixed invalid X_MAX_POS/Y_MAX_POS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1100,12 +1100,12 @@
 #define Y_BED_SIZE 205
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -8.5
+#define X_MIN_POS -8
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 272
-#define Y_MAX_POS 215
-#define Z_MAX_POS 210
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
+#define Z_MAX_POS 205
 
 /**
  * Software Endstops
